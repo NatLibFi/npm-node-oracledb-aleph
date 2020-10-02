b69a096133f50a32ad50de9586ea1618ad17c8b9
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-10-02 10:56:54 +0300

    Add initial implementation

---
+++ script.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# Copyright 2020 University Of Helsinki (The National Library Of Finland)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+if test -z "${PLUGIN_CONFIG_TAG_PATTERN}";then
+  echo 'Setting tag_pattern is mandatory'
+  exit 1
+fi
+
+if test -z "${PLUGIN_CONFIG_NPM_TOKEN}";then
+  echo 'Setting npm_token is mandatory'
+  exit 1
+fi
+
+echo "//registry.npmjs.org/:_authToken=${PLUGIN_CONFIG_NPM_TOKEN}" > ${HOME}/.npmrc
+
+git clone https://github.com/oracle/node-oracledb repo
+cd repo
+
+git checkout `git tag -l ${PLUGIN_CONFIG_TAG_PATTERN}|grep -E '^v[0-9\.]+$'|sort -r|head -n1`
+
+PACKAGE_VERSION=`node -e 'console.log(require("./package").version)'`
+
+if test ${PACKAGE_VERSION} = `npm info @natlibfi/oracledb-aleph version`;then
+  echo 'No changes in upstream, exiting.'
+  exit 0
+fi
+
+git clone -b master --depth=1 https://github.com/oracle/odpi odpi
+
+git apply ../fix-charset.patch
+git apply ../fix-name.patch
+
+npm install
+npm run buildbinary
+npm run buildpackage
+
+echo npm publish "oracledb-${PACKAGE_VERSION}.tgz"
+#npm publish "oracledb-${PACKAGE_VERSION}.tgz"
